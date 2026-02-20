5c3cc205b19b4f86c87c5a3328cc908723ce8580
Pablo García <pgl66@alu.ua.es>
2026-02-20 12:47:37 +0100

    23900722L Barco

---
+++ hada-p2/Barco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HADA {
+    public class Barco {
+    
+        // Propiedades públicas requeridas
+        public Dictionary<Coordenada, string> CoordenadasBarco { get: private AppDomainSetup; }
+        public string Nombre { get; private set; }
+        public int NumeroDe
+
+
+
+    }
+}
