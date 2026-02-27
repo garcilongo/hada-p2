1e3580bf33d59d4e9a069fd4e5d77298112a18db
Raúl Sánchez <rss96@alu.ua.es>
2026-02-27 09:20:32 +0100

    Empiezo Tablero.cs

--- hada-p2/Program.cs
+++ hada-p2/Program.cs
@@ -10,3 +10,6 @@ namespace hada_p2 {
         }
     }
 }
+
+
+
---
+++ hada-p2/Tablero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    internal class Tablero
+    {
+        private int _tamTablero;
+        private List<Coordenada> coordenadasDisparadas;
+        private List<Coordenada> coordenadasTocadas;
+        private List<Barco> barcos;
+        private Dictionary<Coordenada, string> casillasTablero;
+        public int TamTablero
+        {
+            get
+            {
+                return _tamTablero;
+            }
+
+            set
+            {
+                if (value < 4 || value > 9)
+                {
+                    throw new ArgumentOutOfRangeException(); 
+                }
+                
+                _tamTablero = value;
+            }
+
+
+        }
+
+        public Tablero(int tamtablero, List<Barco> barcos)
+        {
+            this.tamTablero = tamtablero;
+            this.barcos = new List<Barcos>();
+
+            for (int i = 0; i < barcos.Count; i++)
+            {
+                this.barcos.Add(barcos[i]);
+            }
+        }
+    }
+}
