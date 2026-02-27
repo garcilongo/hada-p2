d65b8eb647ccf56ac328dca934f962d18b9b40e8
Raúl Sánchez <rss96@alu.ua.es>
2026-02-27 11:10:07 +0100

    Creado game, modificado coordenadas y tablero

---
+++ hada-p2/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    internal class Game
+    {
+        private bool finPartida;
+
+        private void GameLoop()
+        {
+
+        }
+
+        private void CuandoEventoFinPartida(EventArgs e)
+        {
+
+        }
+    }
+}
--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -13,6 +13,9 @@ namespace Hada
         private List<Coordenada> coordenadasTocadas;
         private List<Barco> barcos;
         private Dictionary<Coordenada, string> casillasTablero;
+
+        public event EventHandler<EventArgs> eventoFinPartida;
+
         public int TamTablero
         {
             get
@@ -33,15 +36,70 @@ namespace Hada
 
         }
 
+        private void InicializaCasillasTablero()
+        {
+            for (int fila = 0; fila < this.TamTablero; fila++)
+            {
+
+                for (int columna = 0; columna < this.TamTablero; columna++)
+                {
+                    Coordenada aux = new Coordenada(fila, columna);
+
+                    String valor = "AGUA";
+
+                    for (int i = 0; i < this.barcos.Count; i++)
+                    {
+                        if (this.barcos[i].CoordenadasBarco.ContainsKey(aux))
+                        {
+                            valor = this.barcos[i].Nombre;
+                            break;
+                        }
+                    }
+
+                    this.casillasTablero.Add(aux, valor);
+                }
+            }
+        }
+
+        private void CuandoEventoTocado()
+        {
+
+        }
+
+        private void CuandoEventoHundido()
+        {
+
+        }
+
         public Tablero(int tamtablero, List<Barco> barcos)
         {
-            this.tamTablero = tamtablero;
-            this.barcos = new List<Barcos>();
+            this._tamTablero = tamtablero;
+            this.barcos = new List<Barco>();
+            this.casillasTablero = new Dictionary<Coordenada, string>();
+            this.coordenadasDisparadas = new List<Coordenada>();
+            this.coordenadasTocadas = new List<Coordenada>();
+
 
             for (int i = 0; i < barcos.Count; i++)
             {
                 this.barcos.Add(barcos[i]);
             }
         }
+
+        public void Dispara(Coordenada c)
+        {
+            
+        }
+
+        public string DibujarTablero()
+        {
+
+        }
+
+        public string ToString()
+        {
+
+        }
+
     }
 }
